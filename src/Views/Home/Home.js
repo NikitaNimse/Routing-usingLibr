7e429b4966e43a15190725ab1e31d2627d312080
NikitaNimse <nimsenikita710>
2024-08-24 18:46:53 +0530

    toaster add

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,12 +1,25 @@
 import Navbar from "./../../Components/Navbar/Navbar.js";
 import  "./Home.css"
 import homeimg from "./Home.png"
+import toast, { Toaster } from "react-hot-toast";
 
 const Home = () =>{
     return (<>
      <Navbar/>
     <h1 className="title"> Home page</h1>
-    <img className="img" src={homeimg} />
+    <img className="img" src={homeimg} /> 
+
+    <button type="button" onClick={()=>{
+        toast.loading("please wait...")
+    }}> loading
+    </button>
+
+    <button type="button" onClick={()=>{
+            toast.dismiss()
+    toast.success("Welcome to Home page")
+    }}> Finish
+        </button>
+    <Toaster/>
     </>)
 }
 export default Home
